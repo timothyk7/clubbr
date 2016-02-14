0e1d227a04183d7b40627f5b7c98fd744bf135a6
Timothy Kua <timothyk7@gmail.com>
2016-02-13 21:06:06 -0800

    fixed club profile page

--- routes/clubprofile.js
+++ routes/clubprofile.js
@@ -1,8 +1,8 @@
 var clubs = require('../clubs.json');
 
 exports.view = function(req, res){
-	//var clubID = req.params.id;
-	var clubID = 0;
+	var clubID = req.query.id;
+	// var clubID = 0;
   	var clubInfo = clubs['clubs'][clubID]; // of by one, our first project has index 0
   	console.log(clubInfo);
 	res.render('clubprofile', clubInfo);
