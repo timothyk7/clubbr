bb79994863bcf86e7e2d44f49d2bbbbac27bc529
Wendy Tang <wenhuitang0521@gmail.com>
2016-02-24 20:47:08 -0800

    made it so profile changes are persistent

--- public/js/profile_edit.js
+++ public/js/profile_edit.js
@@ -25,7 +25,8 @@ function initializePage() {
 	$("#editEmail").click(editEmail);
 	$("#change-name").click(editNameHelper);
 	$("#change-email").click(editEmailHelper);
-	$("#saveChanges").click(addInterestsHelper);
+	$("#saveInterests").click(addInterestsHelper);
+	$("#saveChanges").click(saveChanges);
 }
 
 
@@ -45,18 +46,14 @@ function addInterestsHelper(){
 
 
 	var checkboxes = $("input[type='checkbox']");
-	for (var i=0; i<checkboxes.length; i++)  {
-		if (checkboxes[i].type == 'checkbox')   {
-			checkboxes[i].checked = false;
-		}
-	}
+
 	console.log(checkboxes.length);
 	for (var i=0; i<checkboxes.length; i++)  {
 			if (checkboxes[i].type == 'checkbox' && checkboxes[i].checked ) {
 				var checkName = checkboxes[i].value;
 				var del_counter = "#delete-button" + checkboxes[i].id;
 				if(addedInterests.indexOf(del_counter) == -1 ){
-					var listItem = '<li style="margin: 5px;">'+checkName+
+					var listItem = '<li style="margin: 5px;">'+ '<span class="pinterest">' + checkName + '</span>' +
 					'<button id="delete-button'+checkboxes[i].id+
 					'" class="btn btn-xs interests" style="margin-left: 10px;">Delete</button></li>';
 					$('#profile-interests').append(listItem);
@@ -67,7 +64,11 @@ function addInterestsHelper(){
 		}
 	}
 
-
+	for (var i=0; i<checkboxes.length; i++)  {
+		if (checkboxes[i].type == 'checkbox')   {
+			checkboxes[i].checked = false;
+		}
+	}
 
 }
 
@@ -83,7 +84,7 @@ function editNameHelper(){
 
 	if(example){
 		example.fonts
-		$('#profile-name').text(example);
+		$('#pname').text(example);
 		$('#name-modal').modal("hide");
 	} else {
 		$('#name-modal').parents('p').addClass('warning')
@@ -101,7 +102,7 @@ function editEmailHelper(){
 	var example = $('#new-email').val();
 
 	if($('#new-email').val()){
-		$('#profile-email').text(example);
+		$('#pemail').text(example);
 		$('#email-modal').modal("hide");
 	} else {
 		$('#email-modal').parents('p').addClass('warning')
@@ -118,4 +119,45 @@ function deleteInterests(e){
 	    addedInterests.splice(index, 1);
 	}
 	$(this).closest('li').remove();
+}
+
+function saveChanges(e) {
+	var id = getParameterByName('auth');
+	var name = $('#pname').text();
+	var email = $('#pemail').text();
+	var interests = [];
+
+	$('.pinterest').each(function( index ) {
+		var id = '#'+$(this).next().attr('id');
+		var index = addedInterests.indexOf(id);
+
+		var result = {}
+  		result["id"] = index;
+  		result["interest"] = $(this).text();
+  		result["remove_space"] = $(this).text().split(' ').join('_');
+  		interests.push(result);
+
+	});
+
+	var json = {
+		'userid': id,
+		'name': name,
+		'email': email,
+		'interests': JSON.stringify(interests)
+	};
+
+	console.log(json);
+	$.post('/profile/save-changes', json, function() {
+		window.location.href = '/profile'+'?auth='+id; // reload the page
+	});
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
